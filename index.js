5de0d098119f7902a180d701afb15491f9ac7f85
tsettle44 <tsettle44@gmail.com>
2019-02-28 17:57:57 -0500

    add body-parser and cors

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//Middleware
+app.use(bodyParser.json());
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
