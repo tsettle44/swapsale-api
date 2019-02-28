c56775901d491fbe0212955e5d34fba8ea2ea6aa
tsettle44 <tsettle44@gmail.com>
2019-02-28 18:11:07 -0500

    connect to heroku postgres

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+client.connect();
+
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
 
 //Middleware
 app.use(bodyParser.json());
