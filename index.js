d50bf8793c132aa607134ddca62baf9270e13575
tsettle44 <tsettle44@gmail.com>
2019-02-28 18:15:46 -0500

    test

--- index.js
+++ index.js
@@ -11,23 +11,22 @@ const client = new Client({
 
 client.connect();
 
-client.query(
-  "SELECT table_schema,table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  }
-);
-
 //Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", function(req, res) {
   res.send("Hello World!");
+  client.query(
+    "SELECT table_schema,table_name FROM information_schema.tables;",
+    (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    }
+  );
 });
 
 const port = process.env.PORT || 5000;
