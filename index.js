a9593f452a1ec7b782ab13d6ece1191426319eea
tsettle44 <tsettle44@gmail.com>
2019-02-28 22:35:58 -0500

    localhost connection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { Client } = require("pg");
+require("dotenv").config();
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
