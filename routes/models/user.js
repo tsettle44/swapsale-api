5f0af7d10fdedf7e7ac5e8b538822538e05555d8
Tom Settle <tsettle44@gmail.com>
2019-05-01 11:52:35 -0400

    hash password

--- routes/models/user.js
+++ routes/models/user.js
@@ -14,6 +14,18 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
+//hash password
+UserSchema.pre("save", function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
+
 const User = mongoose.model("User", UserSchema);
 
 module.exports.User = User;
